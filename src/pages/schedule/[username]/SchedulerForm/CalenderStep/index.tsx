f366aad47e3545eef06ce874c13f03bbb74726d0
Augusto Gomes <augusto.tgomes@gmail.com>
2023-12-09 02:59:17 -0300

    Created a little os structure for Schedul pPage

---
+++ src/pages/schedule/[username]/SchedulerForm/CalenderStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/components/Calendar'
+import { Container } from './styles'
+
+export function CalenderStep() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
+}
--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,6 +2,7 @@ import { Avatar, Heading, Text } from '@augustogomes-ui/react'
 import { Container, UserHead } from './styles'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '@/lib/prisma'
+import { ScheduleForm } from './SchedulerForm'
 
 interface ScheduleProps {
   user: {
@@ -19,6 +20,8 @@ export default function Schedule({ user }: ScheduleProps) {
         <Heading>{user.name}</Heading>
         <Text>{user.bio}</Text>
       </UserHead>
+
+      <ScheduleForm />
     </Container>
   )
 }
